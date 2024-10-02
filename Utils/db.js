5ef1069d6c1e3483c3eeb62149395abc1dee5d2a
Davidokih <davidyole023@gmail.com>
2024-10-02 10:32:24 +0100

    update

--- Utils/db.js
+++ Utils/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const url = "mongodb://0.0.0.0:27017/Chat_App";
+const url = process.env.LOCAL_URL2;
 
 mongoose.connect(url).then(() => {
     console.log("Connected to DataBase");
